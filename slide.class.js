e4041c179ebe8af9ef74abf2c06ebca3140469b2
Moppy-cloud <76014643+Moppy-cloud@users.noreply.github.com>
2020-12-20 19:15:02 +0100

    Create slide.class.js

---
+++ slide.class.js
@@ -0,0 +1,22 @@
+export default class Slide{
+    constructor(main = '#application'){
+        this.main = document.querySelector(main);
+        if(!this.main){
+            this.main = document.createElement('main');
+            this.main.id = 'application';
+        }
+        this.listen = {};
+        this.changeVariable = (name, value) => this.main.querySelectorAll('*[data-listener-var="' + name + '"]').forEach(i => i.innerHTML = value);
+    }
+    setGlobal(name, value){
+        this.changeVariable(name, value);
+        this.listen[name] = value;
+    }
+    render(block){
+        this.main.innerHTML = block;
+        Object.keys(this.listen).forEach(key => [...this.main.querySelectorAll('*')].filter(i => i.innerHTML.includes(`{${key}}`)).forEach(i => {
+            i.setAttribute('data-listener-var', key);
+            i.innerHTML = i.innerHTML.replaceAll(`{${key}}`, this.listener[key])
+        }));
+    }
+}
